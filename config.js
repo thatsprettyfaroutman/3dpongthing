dcc017872f8c2a8ae72d28bce8dcaad927228547
Viljami Anttonen <viljami.anttonen@gmail.com>
2015-01-13 00:14:43 +0200

    Started using es6, bit crappy atm but works

--- config.js
+++ config.js
@@ -1,7 +1,30 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "3dpong/*": "lib/*.js"
+    "3dpong/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "three": "npm:three@0.69.0",
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.0.1"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:buffer@3.0.1": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.4",
+      "is-array": "npm:is-array@1.0.1"
+    },
+    "npm:three@0.69.0": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    }
   }
 });
 
